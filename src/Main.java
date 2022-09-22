689ca6bdcfd27345d532802472d60d2cc3cfba64
RyanCap03 <capre@mail.uc.edu>
2022-09-22 13:06:37 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+     int bMonth = 6;
+
+    if (bMonth >= 1 && bMonth <=12)
+    {
+        System.out.println("Your birth month is: " + bMonth);
+    }
+    else
+    {
+        System.out.println("That is not a birth month value! try again");
+    }
+
+
+    }
+}
